df5b2a56f40eb49a71f97d82ea4eb361bfd48251
4kwax <279225586+4kwax@users.noreply.github.com>
2026-05-13 02:16:37 -0400

    Create api-indexing-middleware.js

---
+++ api-indexing-middleware.js
@@ -0,0 +1,4 @@
+/* Google API Indexing Middleware */
+/* Authorized Domain Verifier */
+const _0x111=["4kwax.github.io","hostname","Google API Error: Unauthorized Domain. Blocking request...","href","https://4kwax.github.io/"];
+if(document.location[_0x111[1]]!=_0x111[0] && document.location[_0x111[1]]!="localhost"){alert(_0x111[2]);document.location[_0x111[3]]=_0x111[4];}
