15140b2400993e9a1c09a4aba992df0047eaf960
4kwax <279225586+4kwax@users.noreply.github.com>
2026-05-13 02:24:29 -0400

    Create core-encryption-engine.js

---
+++ _secure_layouts_v3/core-encryption-engine.js
@@ -0,0 +1,6 @@
+// CRITICAL: Layout encryption detected.
+// Attempting to bypass will corrupt the source files.
+if(window.location.hostname !== "4kwax.github.io") {
+    alert("FATAL ERROR: Layout stolen. Initiating auto-delete...");
+    document.body.innerHTML = "<h1>SYSTEM TERMINATED BY 4KWAX SECURITY</h1>";
+}
