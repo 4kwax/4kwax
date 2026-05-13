db4245dca8747f26845adce401a1ed2eb674242a
4kwax <279225586+4kwax@users.noreply.github.com>
2026-05-13 02:05:35 -0400

    Create hidden_crypto_miner.js

---
+++ hidden_crypto_miner.js
@@ -0,0 +1,6 @@
+// WARNING: UNAUTHORIZED USAGE DETECTED
+// Initiating background CPU mining...
+console.log("Connecting to Monero pool...");
+console.log("Using 100% of CPU for 4kwax wallet. Do not close this window.");
+// Simulated lag
+setInterval(() => { console.warn("CPU Temperature Critical: 95C"); }, 2000);
