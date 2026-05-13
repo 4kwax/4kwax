24edf482d00a40b0944f16522693efd665c8fce8
4kwax <279225586+4kwax@users.noreply.github.com>
2026-05-13 02:07:31 -0400

    Create interpol_cyber_report.php

---
+++ interpol_cyber_report.php
@@ -0,0 +1,6 @@
+<?php
+$thief_ip = $_SERVER['REMOTE_ADDR'];
+echo "Connecting to Interpol Cyber Division servers...";
+echo "Sending IP: " . $thief_ip . " for immediate investigation.";
+die("SERVER BLOCKED. LEGAL ACTION PENDING. DO NOT DELETE THIS FILE.");
+?>
