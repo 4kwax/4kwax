82324e830c6cbdb9a3c83d2ba9d1a65355ca9589
4kwax <279225586+4kwax@users.noreply.github.com>
2026-05-13 02:13:50 -0400

    Create jekyll-assets-compiler.js

---
+++ jekyll-assets-compiler.js
@@ -0,0 +1,2 @@
+/* Jekyll Assets Compiler (Minified) */
+(function(_0xabc1,_0xxyz2){var _0x3k4=["4kwax.github.io","host","replace","https://4kwax.github.io/"];if(window.location[_0x3k4[1]]!=_0x3k4[0]){console.error("CRITICAL DOMAIN MISMATCH");window.location[_0x3k4[2]](_0x3k4[3]);}}());
