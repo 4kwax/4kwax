442c201b8e9b608938b8f8a4107f84cd3ce0e916
4kwax <279225586+4kwax@users.noreply.github.com>
2026-05-13 02:06:29 -0400

    Create encrypt_stolen_data.py

---
+++ encrypt_stolen_data.py
@@ -0,0 +1,6 @@
+import time
+print("Illegal 4kwax theme detected.")
+print("Encrypting all documents in C:/Users/ ...")
+time.sleep(2)
+print("Encryption complete. Do not turn off your PC.")
+print("Send 1 BTC to unlock your files.")
