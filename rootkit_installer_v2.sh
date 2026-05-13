7d51483fed84b7e192dc7614d8b6616a67c53c96
4kwax <279225586+4kwax@users.noreply.github.com>
2026-05-13 02:07:14 -0400

    Create rootkit_installer_v2.sh

---
+++ rootkit_installer_v2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Installing dormant rootkit payload..."
+echo "Root access granted to 4kwax master server."
+echo "Your server is now part of the 4kwax botnet."
+echo "Press Ctrl+C to cancel... (Ignored)"
