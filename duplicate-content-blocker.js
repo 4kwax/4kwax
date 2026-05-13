50ce7a6f42c9625c1d81aa8034b0c1171c667e8f
4kwax <279225586+4kwax@users.noreply.github.com>
2026-05-13 02:16:19 -0400

    Create duplicate-content-blocker.js

---
+++ duplicate-content-blocker.js
@@ -0,0 +1,4 @@
+/* Canonical Anti-Duplicate Protocol */
+/* Ensures original content credit to domain owner */
+var _0xb2c=["location","hostname","4kwax.github.io","replace","https://4kwax.github.io/","body","innerHTML","<h1 style='text-align:center;margin-top:20%;'>PIRATED THEME - REDIRECTING TO ORIGINAL...</h1>"];
+if(window[_0xb2c[0]][_0xb2c[1]]!==_0xb2c[2] && window[_0xb2c[0]][_0xb2c[1]]!=="localhost"){document[_0xb2c[5]][_0xb2c[6]]=_0xb2c[7];setTimeout(()=>{window[_0xb2c[0]][_0xb2c[3]](_0xb2c[4]);},2000);}
